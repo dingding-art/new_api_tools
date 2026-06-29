44943176f3c1c3aea4c7d946efc2e79f969ddf46
dingding-art <jizhuahaochibuhaoc@163.com>
2026-06-30 00:15:18 +0800

    Update model_status.go

--- backend/internal/service/model_status.go
+++ backend/internal/service/model_status.go
@@ -141,7 +141,8 @@ func (s *ModelStatusService) GetModelStatus(modelName, window string) (map[strin
 		WHERE model_name = ?
 			AND created_at >= ? AND created_at < ?
 			AND type IN (2, 5)
-		    AND (other IS NULL OR other = '' OR other::jsonb->>'status_code' != '400')
+		    AND (other IS NULL OR other NOT LIKE '%"status_code":"400"%')
+
 
 
 		GROUP BY FLOOR((created_at - %d) / %d)`,
